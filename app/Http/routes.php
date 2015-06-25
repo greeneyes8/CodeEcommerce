b2719603cbb35d533c9bfe798fb7d7dd237705c3
unknown <desenvolvimento03@CRMNOTE_6.crmsolucoes.local>
2015-06-25 08:47:03 -0300

    Entrega 3 - Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,29 @@
 <?php
 
-Route::get('/', function () {
+Route::get('/', function ()
+{
     return view('welcome');
 });
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
+Route::group(['prefix' => 'admin'], function ()
+{
+    Route::pattern('id', '[0-9]+');
+
+    Route::get('categories', ['as' => 'admin.categories.index', 'uses' => 'AdminCategoriesController@index']);
+    Route::get('categories/create', ['as' => 'admin.categories.create', 'uses' => 'AdminCategoriesController@create']);
+    Route::post('categories', ['as' => 'admin.categories.store', 'uses' => 'AdminCategoriesController@store']);
+    Route::get('categories/{id}', ['as' => 'admin.categories.show', 'uses' => 'AdminCategoriesController@show']);
+    Route::get('categories/{id}/edit', ['as' => 'admin.categories.edit', 'uses' => 'AdminCategoriesController@edit']);
+    Route::put('categories/{id}', ['as' => 'admin.categories.update', 'uses' => 'AdminCategoriesController@update']);
+    Route::delete('categories/{id}', ['as' => 'admin.categories.destroy', 'uses' => 'AdminCategoriesController@destroy']);
+    //Route::resource('categories', 'AdminCategoriesController');
+
+    Route::get('products', ['as' => 'admin.products.index', 'uses' => 'AdminProductsController@index']);
+    Route::get('products/create', ['as' => 'admin.products.create', 'uses' => 'AdminProductsController@create']);
+    Route::post('products', ['as' => 'admin.products.store', 'uses' => 'AdminProductsController@store']);
+    Route::get('products/{id}', ['as' => 'admin.products.show', 'uses' => 'AdminProductsController@show']);
+    Route::get('products/{id}/edit', ['as' => 'admin.products.edit', 'uses' => 'AdminProductsController@edit']);
+    Route::put('products/{id}', ['as' => 'admin.products.update', 'uses' => 'AdminProductsController@update']);
+    Route::delete('products/{id}', ['as' => 'admin.products.destroy', 'uses' => 'AdminProductsController@destroy']);
+    //Route::resource('products', 'AdminProductsController');
+});
